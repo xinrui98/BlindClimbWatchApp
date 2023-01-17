7d0dac5fcd4ca9d72aa40d2cd330b0bfdff172b6
xinrui98 <gao.xinrui98@gmail.com>
2023-01-17 11:39:31 +0800

    Initial Commit

---
+++ MyWorkouts Watch App/MyWorkoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWorkoutsApp.swift
+//  MyWorkouts Watch App
+//
+//  Created by Xinrui gao on 17/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MyWorkouts_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
